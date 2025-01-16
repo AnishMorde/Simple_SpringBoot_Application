898fe73130cc7a51249629f85328a31ce373dfe7
AnishMorde <anishmorde@gmail.com>
2025-01-16 20:55:59 +0530

    new

--- src/main/java/org/example/helloworld/HelloWorldController.java
+++ src/main/java/org/example/helloworld/HelloWorldController.java
@@ -3,34 +3,22 @@ package org.example.helloworld;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
 public class HelloWorldController {
 
-    private ArrayList<String> items = new ArrayList<>();
+    private List<String> users = new ArrayList<>();
 
-    // GET Mapping: Fetch all items
-    @GetMapping("/items")
-    public ArrayList<String> getItems() {
-        return items; // Returns the list of items
+    @GetMapping
+    public List<String> getAllUsers() {
+        return users;
     }
 
-    // POST Mapping: Add a new item
-    @PostMapping("/items")
-    public String addItem(@RequestBody String item) {
-        items.add(item); // Adds the item to the list
-        return "Item added successfully!";
-    }
-
-    @DeleteMapping("/items")
-    public String delItem(@RequestBody String item){
-        items.remove(item);
-        return "Item delete successfully";
-    }
-
-    @PatchMapping("/items")
-    public String patchItem (@RequestBody String item ){
-        items.set(0,item);
-        return "item patch";
+    @PostMapping
+    public String addUser(@RequestBody String user) {
+        users.add(user);
+        return "User added successfully!";
     }
 }
